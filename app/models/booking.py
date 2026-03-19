5b9139e7194b85e742e0e7d44c020d8ae6e98cba
Kadyr <Nekketsu4@mail.ru>
2026-03-19 20:55:37 +0300

    refactor: refactored User model

--- app/models/booking.py
+++ app/models/booking.py
@@ -32,13 +32,19 @@ class User(Base):
         String(255), unique=True, index=True, nullable=False
     )
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
-    full_name: Mapped[str] = mapped_column(String(255), nullable=False)
+    username: Mapped[str] = mapped_column(String(255), nullable=False)
+    first_name: Mapped[str] = mapped_column(String(255), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(255), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now()
     )
 
     bookings: Mapped[list["Booking"]] = relationship("Booking", back_populates="user")
+
+    @property
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class Route(Base):
